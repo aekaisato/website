ea97a3e862f3feb183209a86c2bb84dadcfcb78f
aekaisato <kingdomkeepersguy@gmail.com>
2022-08-09 12:14:16 -0700

    created about me box

---
+++ src/AboutMeBox.svelte
@@ -0,0 +1,80 @@
+<script>
+  import aboutMeContent from "src/content/about-me.txt?raw";
+
+  const tablerUrl = "https://cdn.jsdelivr.net/npm/@tabler/icons@latest/icons/";
+  const socialLinks = [
+    {
+      name: "GitHub",
+      link: "https://github.com/aekaisato/",
+      icon: "brand-github"
+    },
+    {
+      name: "YouTube",
+      link: "https://www.youtube.com/channel/UCXFcmOeXm7bcxBG3qPEiOkw",
+      icon: "brand-youtube"
+    },
+    {
+      name: "itch.io",
+      link: "https://aekaisato.itch.io/",
+      icon: "device-gamepad-2"
+    }
+  ];
+</script>
+
+<div class="container">
+  <div class="flex content-box">
+    <p class="text-content">
+      {aboutMeContent}
+    </p>
+  </div>
+  <div class="flex link-box">
+    {#each socialLinks as sl}
+      <div class="flex">
+        <a
+          href={sl.link}
+          target="_blank"
+          rel="noopener noreferrer"
+          class="flex"
+        >
+          <img
+            src={tablerUrl + sl.icon + ".svg"}
+            alt={sl.name}
+            class="invert"
+          />
+        </a>
+      </div>
+    {/each}
+  </div>
+</div>
+
+<style>
+  .container {
+    background-color: black;
+    display: flex;
+    flex: 1;
+    flex-direction: row;
+    justify-content: center;
+    align-self: stretch;
+  }
+  .content-box {
+    flex: 7;
+    overflow-y: scroll;
+  }
+  .link-box {
+    flex: 1;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+  }
+  .flex {
+    display: flex;
+  }
+  .invert {
+    filter: invert();
+  }
+  .text-content {
+    margin: 5%;
+    color: white;
+    font-size: smaller;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,36 @@
 <script>
   import ProjectWheel from "src/components/ProjectWheel.svelte";
+  import AboutMeBox from "src/AboutMeBox.svelte";
 </script>
 
-<div style="overflow: hidden; height: 100vh">
-  <ProjectWheel/>
+<svelte:head>
+  <title>Aiden Sato // aekai</title>
+  <meta name="description" content="Portfolio page for Aiden Sato" />
+</svelte:head>
+
+<div class="container">
+  <ProjectWheel />
+  <div class="about-me">
+    <AboutMeBox />
+  </div>
 </div>
+
+<style>
+  :global(p) {
+    font-family: uncut_sansregular
+  }
+  .container {
+    overflow: hidden;
+    height: 100vh;
+    width: 100vw;
+  }
+  .about-me {
+    position: absolute;
+    right: 3vh;
+    bottom: 3vh;
+    width: 35vw;
+    height: 25vh;
+    display: flex;
+    flex: 1;
+  }
+</style>
